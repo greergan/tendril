f4cc9c5eb3794f9d5d86117a8b8fe27762eac4e3
Jeff Greer <greergan@gmail.com>
2023-12-26 18:20:47 -0800

    changed to allow Configuration object usage

--- src/tendril_queue_manager.cpp
+++ src/tendril_queue_manager.cpp
@@ -1,19 +1,37 @@
+#include <chrono>
+#include <iostream>
+#include <unordered_map>
 #include <tendril/queue/kafka.h>
 #include <tendril/queue/manager.h>
 #include <librdkafka/rdkafkacpp.h>
-bool tendril::queue::manager::initialize(std::string brokers) {
-	return tendril::queue::kafka::initialize(brokers);
+namespace tendril::queue::manager {
+	std::unordered_map<std::string, tendril::queue::QueuePacket> reply_queue;
+}
+bool tendril::queue::manager::initialize(tendril::Configuration& configuration) {
+	return tendril::queue::kafka::initialize(configuration, &reply_queue);
 }
 tendril::queue::QueuePacket tendril::queue::manager::get_some(std::string conversation_id) {
-	tendril::queue::QueuePacket reply;
-	reply.data = "HTTP/1.1 200 OK\r\nContent-Length:20\r\nContent-Type: text/html\r\n\r\n<html>hello</html>\r\n";
-	reply.close_socket = true;
-	return reply;
+	tendril::queue::QueuePacket reply_packet;
+	using namespace std::chrono_literals;
+	auto loop_start_time = std::chrono::steady_clock::now();
+	for(;;) {
+		if(auto packet_tuple = reply_queue.find(conversation_id); packet_tuple != reply_queue.end()) {
+			reply_packet = std::move(packet_tuple->second);
+			reply_queue.erase(conversation_id);
+			return reply_packet;
+		}
+		if(std::chrono::steady_clock::now() > loop_start_time + 4s) {
+			break;
+		}
+	}
+	reply_packet.close_socket = true;
+	return reply_packet;
 }
 bool tendril::queue::manager::send_some(tendril::queue::QueuePacket packet) {
+	//https://github.com/confluentinc/librdkafka/blob/master/examples/rdkafka_example.cpp#L510
 	auto error_code = tendril::queue::kafka::produce(packet);
 	if(error_code) {
-		//std::cerr << "tendril::queue::manager::send_some::error_code: " << error_code << "\n";
+		//std::cerr << "tendril::queue::manager::send_some::error_code: " << RdKafka::err2str(error_code) << std::endl;
 		return false;
 	}
 	return true;
